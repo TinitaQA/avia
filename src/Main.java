6bb38fc8950b7ae61dc743abae5750c02dd55dc7
Tinita <tino4ka.87@mail.ru>
2023-10-11 11:48:20 +0300

    BonusMilesService.java added

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-
-        int ticket = 12120; // стоимость билета
-        int mile = 20; // количество рублей для одной бонусной мили
-
-        int bonus = ticket / mile; // количество бонусных миль
-
-        System.out.println(" количество бонусных миль " + bonus);
+        BonusMilesService service = new BonusMilesService();
+        int price = 10_000;
+        int miles = service.calculate(price); // должно получиться 500
+        System.out.println(miles);
     }
 }
